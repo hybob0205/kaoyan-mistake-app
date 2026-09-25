228978fb9735ad96934fac157c76d94fd03a33e6
hybob0205 <480883128@qq.com>
2026-09-25 15:21:57 +0800

    Match crop overlay to reference guide

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'shicuo-pwa-v18';
-const FILES = ['./', './index.html', './app.css?v=21', './app.js?v=21', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
+const CACHE = 'shicuo-pwa-v19';
+const FILES = ['./', './index.html', './app.css?v=22', './app.js?v=22', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting()));
 });
