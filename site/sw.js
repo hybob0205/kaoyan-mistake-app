fda84f1d5fb28bda8a53b3f56df373f48439cf3d
hybob0205 <480883128@qq.com>
2026-09-25 10:27:59 +0800

    Fix crop interactions and improve chat navigation

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'shicuo-pwa-v10';
-const FILES = ['./', './index.html', './app.css?v=12', './app.js?v=11', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
+const CACHE = 'shicuo-pwa-v11';
+const FILES = ['./', './index.html', './app.css?v=13', './app.js?v=12', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting()));
 });
